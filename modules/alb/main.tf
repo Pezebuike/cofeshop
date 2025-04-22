54a1902d7e544ceb923e491548e7c63b8affea29
ezebuike michael <99094143+Pezebuike@users.noreply.github.com>
2025-04-22 10:12:34 +0100

    set internal parameter to true

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,7 +1,7 @@
 # create application load balancer
 resource "aws_lb" "application_load_balancer" {
   name               = "${var.project_name}-alb"
-  internal           = false
+  internal           = true
   load_balancer_type = "application"
   security_groups    = [var.alb_security_group_id]
   subnets            = [var.public_subnet_az1_id, var.public_subnet_az2_id]
